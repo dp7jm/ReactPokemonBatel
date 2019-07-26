28fb4510765b40e266b57872649a99e527b51658
{dp7jm} <{dpjm@chef.net}>
2019-07-26 21:36:05 +0200

    added pokemon image to the pokecard

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './Pokecard.css';
 
+
 class Pokecard extends Component {
     render(){
         return(
             <div>
                 <h1>{this.props.name}</h1>
+                <img src = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png'/>
                 <p>{this.props.id}</p>
                 <p>{this.props.experience_point}</p>
 
