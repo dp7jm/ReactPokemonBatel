49746ac31b6dba8ef20a4990ec2270c0c4aa1a21
{dp7jm} <{dpjm@chef.net}>
2019-07-27 01:16:30 +0200

    pokedex card displayed from polelist

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,28 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Pokedex.css';
 import Pokecard from './Pokecard';
 import Pokemonlist from './Pokemonlist';
 
 class Pokedex extends Component {
     static defaultProps = {
-        pokemonlist: Pokemonlist 
+        pokemonlist: Pokemonlist
     }
     render() {
         const pokemonList = this.props.pokemonlist;
-        return(
-            
+        return (
+
             <div>
                 <h1>Poke DEX</h1>
-                {console.log(Pokemonlist)}
-                {console.log(this.props.pokemonlist)}
-                {console.log(this.props.pokemonlist[0].id)}
+                {
+                    pokemonList.map((p,prop) => (
+                        <Pokecard
+                            key={prop}                        
+                            id={p.id}
+                            name={p.name}
+                            type={p.type}
+                            exp={p.exp}
+                        />
+                    ))
 
-                <Pokecard 
-                id = {pokemonList[0].id}
-                name = {pokemonList[0].name} 
-                type = {pokemonList[0].type} 
-                exp = {pokemonList[0].exp} 
-                />
+                }
+
+                {/* 
+                <Pokecard
+                    id={pokemonList[0].id}
+                    name={pokemonList[0].name}
+                    type={pokemonList[0].type}
+                    exp={pokemonList[0].exp}
+                /> */}
 
 
             </div>
