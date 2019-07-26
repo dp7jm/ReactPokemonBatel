bfab0f9f83cfb0fea9a0d51876b4050047de45df
{dp7jm} <{dpjm@chef.net}>
2019-07-26 23:51:38 +0200

    added default props to Pokedex component

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import './Pokedex.css';
 import Pokecard from './Pokecard';
+import Pokemonlist from './Pokemonlist';
 
 class Pokedex extends Component {
+    static defaultProps = {
+        pokemonlist: Pokemonlist 
+    }
     render() {
+        // const pokemonList = this.props.pokemonlist;
         return(
+            
             <div>
                 <Pokecard 
-                id = {4}
+                id = {2}
                 name = "Charmander" 
                 type = "fire"
                 exp = {62}
--- src/Pokemonlist.js
+++ src/Pokemonlist.js
@@ -6,7 +6,7 @@ const pokemonList = [
     {id: 35, name: 'Charmander', type: 'fire', exp: 63},
     {id: 44, name: 'Squirle', type: 'water', exp: 63},
     {id: 21, name: 'Metapod', type: 'bug', exp: 72},
-    {id: 18, name: 'Pikachu', type: 'electric', exp: 112},
+    {id: 18, name: 'Pikachu', type: 'electric', exp: 112}
 
 ]
 export default pokemonList;
