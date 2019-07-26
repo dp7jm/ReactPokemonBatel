e0d9e09edc33a5f5e1e97c5d42d60ec4b02e51bd
{dp7jm} <{dpjm@chef.net}>
2019-07-26 20:25:16 +0200

    creted templet Pokecard and Pokedex component

---
+++ src/Pokecard.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Pokecard extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+
+}
+
+export default Pokecard;
