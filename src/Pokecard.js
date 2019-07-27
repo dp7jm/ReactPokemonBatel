e2190abceb5e60248e638c757bca2b1e0bf07803
{dp7jm} <{dpjm@chef.net}>
2019-07-27 04:43:01 +0200

    pokemon id is now responsibel for generating image

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,12 +2,25 @@ import React, {Component} from 'react';
 import './Pokecard.css';
 const imageSource = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
+const adres = (id) => {
+    if (id<10) {
+        return `${imageSource}00${id}.png`;
+    }  
+    else if (id<100) {
+        return `${imageSource}0${id}.png`;
+    }  
+    else {
+        return `${imageSource}${id}.png`;
+       
+    }
+}
 class Pokecard extends Component {
     render(){
+        const {name, id, exp, type} = this.props;
         return(
             <div className = "Pokecard">
                 <h1>{this.props.name}</h1>
-                <img className = "Pokecard-img" src = {`${imageSource}009.png`} alt = {this.props.name}/>
+                <img className = "Pokecard-img" src = {`${adres(this.props.id)}`} alt = {this.props.name}/>
                 <div>{this.props.id}</div>
                 <div>{this.props.exp}</div>
 
