00b822958dbfdad204125fa95b7fc80696ea2d45
{dp7jm} <{dpjm@chef.net}>
2019-07-27 05:29:31 +0200

    creted function fore chosinge pokemon from list

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
-
+import Pokemonlist from './Pokemonlist';
+const chose = (Pokemonlist, numberOf) => {
+    this.numberOf = 4;
+    let newArry = [];
+   for (let index = 0; index < numberOf; index++) {
+    newArry.push(Pokemonlist[Math.random]);
+          
+   } 
+   return newArry;
+}
 class Pokegame extends Component {
     render() {
+        
         return(
             <div>
-                
+
+
             </div>
-        )
+        );
     }
 }
+export default Pokegame;
