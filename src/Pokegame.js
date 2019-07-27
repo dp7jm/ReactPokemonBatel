1e622e07907660ae381cb0f88b792f3cf870aa07
{dp7jm} <{dpjm@chef.net}>
2019-07-27 05:47:49 +0200

    two pokedex witch 4 pokemons

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,19 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Pokemonlist from './Pokemonlist';
-const chose = (Pokemonlist, numberOf) => {
-    this.numberOf = 4;
+const chose = (Pokemonlis, numberOf) => {
     let newArry = [];
-   for (let index = 0; index < numberOf; index++) {
-    newArry.push(Pokemonlist[Math.random]);
-          
-   } 
-   return newArry;
+    for (let index = 0; index < numberOf; index++) {
+        newArry.push(Pokemonlis[Math.floor(Math.random() * Pokemonlis.length)]);
+
+    }
+    return newArry;
 }
 class Pokegame extends Component {
     render() {
-        
-        return(
+        const firstHand = chose(Pokemonlist, 4);
+        const secondHand = chose(Pokemonlist, 4);
+
+        return (
             <div>
+                {console.log(firstHand)}
 
 
             </div>
